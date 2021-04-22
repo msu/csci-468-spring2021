dd16deebd76332ddeded156c87c15aaf8898c024
Tim-Bender <57515383+Tim-Bender@users.noreply.github.com>
2021-04-21 21:34:55 -0600

    Added ++ and --

--- src/main/java/edu/montana/csci/csci468/parser/statements/AssignmentStatement.java
+++ src/main/java/edu/montana/csci/csci468/parser/statements/AssignmentStatement.java
@@ -7,7 +7,6 @@ import edu.montana.csci.csci468.parser.ErrorType;
 import edu.montana.csci.csci468.parser.ParseError;
 import edu.montana.csci.csci468.parser.SymbolTable;
 import edu.montana.csci.csci468.parser.expressions.Expression;
-import org.objectweb.asm.Opcodes;
 
 public class AssignmentStatement extends Statement {
     private Expression expression;
@@ -47,7 +46,8 @@ public class AssignmentStatement extends Statement {
     //==============================================================
     @Override
     public void execute(CatscriptRuntime runtime) {
-        runtime.setValue(variableName, expression.evaluate(runtime));
+        runtime.setValue(variableName,expression.evaluate(runtime));
+        //super.execute(runtime);
     }
 
     @Override
@@ -57,31 +57,6 @@ public class AssignmentStatement extends Statement {
 
     @Override
     public void compile(ByteCodeGenerator code) {
-
-        Integer slotforvar = code.resolveLocalStorageSlotFor(getVariableName());
-        if (slotforvar != null) {
-            expression.compile(code);
-            if (expression.getType() == CatscriptType.INT || expression.getType() == CatscriptType.BOOLEAN) {
-                code.addVarInstruction(Opcodes.ISTORE, slotforvar);
-            } else {
-                code.addVarInstruction(Opcodes.ASTORE, slotforvar);
-            }
-        } else {
-            code.addVarInstruction(Opcodes.ALOAD, 0);
-            expression.compile(code);
-            if (expression.getType() == CatscriptType.OBJECT) {
-                box(code, expression.getType());
-            }
-            if (expression.getType() == CatscriptType.INT) {
-                code.addFieldInstruction(Opcodes.PUTFIELD, getVariableName(), "I", code.getProgramInternalName());
-            } else if (expression.getType() == CatscriptType.BOOLEAN) {
-                code.addFieldInstruction(Opcodes.PUTFIELD, getVariableName(), "Z", code.getProgramInternalName());
-            } else if (expression.getType() == CatscriptType.NULL) {
-                code.addFieldInstruction(Opcodes.PUTFIELD, getVariableName(), "Ljava/lang/Object;", code.getProgramInternalName());
-            } else {
-                code.addFieldInstruction(Opcodes.PUTFIELD, getVariableName(), "L" + ByteCodeGenerator.internalNameFor(
-                        expression.getType().getJavaType()) + ";", code.getProgramInternalName());
-            }
-        }
+        super.compile(code);
     }
 }
